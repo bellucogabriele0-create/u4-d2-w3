f25aab90d2025fa8044b4dcbaa91ab7b1b2ca07c
bellucogabriele0-create <bellucogabriele0@gmail.com>
2026-01-20 15:41:44 +0100

    Initial commit

---
+++ src/main/java/gabrielebelluco/Application.java
@@ -0,0 +1,20 @@
+package gabrielebelluco;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+    // dopo aver aggiunto le dependencies in pom.xml e le properties in persistence.xml
+    // aggiungiamo nel main una entity manager factory
+    // cioè un oggetto che creerà gli entity manager che mi permetteranno di interagire con DB
+    // senza di essi non si collega al DB come un app normale senza DB. aggiungendo questo:
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4d2w3");
+    // una volta inserito se si avvia il risultato sarà un'errore perchè il server non esiste,
+    // quindi ora si va a crearlo in pgAdmin Databases>tasto destro> create> database> u4-d1-w3 > save
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
